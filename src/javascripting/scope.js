84cc09ab2966aacb99bf88baea32192374b644f6
Feross Aboukhadijeh <feross@feross.org>
2019-10-09 14:06:37 -0700

    include eslint disable comment

--- src/javascripting/scope.js
+++ src/javascripting/scope.js
@@ -1,1 +1,6 @@
+// The following comment disables lint errors for unused variables since your
+// solution to this exercise should have unused variables in it :)
+
+/* eslint-disable no-unused-vars */
+
 // TODO: Replace this with your solution.
